878b3b9de88a70325c9a199c82b1e2d9279be8bd
gongliang <gongliang@aprbrother.com>
2014-08-27 15:58:09 +0800

    add addressdatamanager

---
+++ AddressBook/AddressDataManager.swift
@@ -0,0 +1,39 @@
+//
+//  AddressDataManager.swift
+//  AddressBook
+//
+//  Created by gongliang on 14-8-27.
+//  Copyright (c) 2014年 AB. All rights reserved.
+//
+
+import UIKit
+
+class AddressDataManager: NSObject {
+
+    class func peopleDatas() -> NSArray {
+        if let array: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("people") {
+            var datasArray: NSMutableArray = []
+            array.enumerateObjectsUsingBlock ( { (dic, index, stop) -> Void in
+                let number = dic["number"] as String
+                let name = dic["name"] as String
+                let tel = dic["telePhone"] as String
+                let people = People(number: number.toInt()!, name: name, telePhone: tel)
+                datasArray.addObject(people)
+            })
+            return datasArray as NSArray
+        } else {
+            return []
+        }
+    }
+    
+    class func savePeople(people: People) {
+        if let array: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("people") {
+            let dic: Dictionary = ["number": "\(people.number)",
+                                     "name": people.name,
+                                "telePhone": people.telePhone]
+            array.addObject(dic)
+            NSUserDefaults.standardUserDefaults().setObject(array, forKey: "people")
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+}
